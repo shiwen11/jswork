1ff3b4eb690d535f0f83b1d59b6ad4e96f75817e
shiwen11 <55181157+shiwen11@users.noreply.github.com>
2019-10-16 15:43:45 +0800

    Add files via upload
    work6

---
+++ work6/pai.js
@@ -0,0 +1,31 @@
+function calculate(){
+    let num = document.getElementById('').nodeValue
+    num = parseInt(num)&&Number(num)
+    if(!isNaN(num)){
+        document.getElementById('').value = pi()
+    }
+}
+
+function factorial(n){
+    let sum =1 
+    for(){
+    }
+ return sum
+}
+function oddFactorial(n){
+    let sum = 1
+    for(let ){
+
+    }
+    return sum
+}
+function pi(num){
+    let sum =0
+    for(let i=0;i<=num;++i){
+
+    let dividend = factorial()
+    let divisor=oddFactorial()
+    }
+    return 
+}
+ calculate()
